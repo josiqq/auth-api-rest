05d0595551102695bab415908933fdd52e56edd1
jositsk <josiwasd@gmail.com>
2024-06-17 08:22:21 -0400

    first commit

--- dist/routes/default.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const default_controllet_1 = require("../controllers/default.controllet");
-const router = (0, express_1.Router)();
-router.get('/', default_controllet_1.defaultController);
-exports.default = router;
